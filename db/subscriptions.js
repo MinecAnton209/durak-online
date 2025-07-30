a4c0f413ab3f10f14e178659f3059be05c7291e6
MinecAnton209 <113017881+MinecAnton209@users.noreply.github.com>
2025-07-30 21:23:16 +0300

    Update subscriptions.js

--- db/subscriptions.js
+++ db/subscriptions.js
@@ -30,7 +30,13 @@ async function findSubscriptionByUserId(userId) {
         const query = `SELECT subscription_data FROM push_subscriptions WHERE user_id = ?;`;
         db.get(query, [userId], (err, row) => {
             if (err) return reject(err);
-            resolve(row ? JSON.parse(row.subscription_data) : null);
+            if (!row) return resolve(null);
+
+            const data = typeof row.subscription_data === 'string' 
+                ? JSON.parse(row.subscription_data) 
+                : row.subscription_data;
+            
+            resolve(data);
         });
     });
 }
