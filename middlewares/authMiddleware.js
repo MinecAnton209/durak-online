f22929ad9a85a3c6056000c74a8c0b6d36ac0bf9
MinecAnton209 <113017881+MinecAnton209@users.noreply.github.com>
2025-10-08 22:53:24 +0300

    Revert "Refactor monolithic server.js into a modular structure"

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-function ensureAdmin(req, res, next) {
+﻿function ensureAdmin(req, res, next) {
     if (req.session.user && req.session.user.is_admin) {
         return next();
     }
@@ -8,38 +8,31 @@ function ensureAdmin(req, res, next) {
         i18nKey: 'error_forbidden_admin_only'
     });
 }
+function ensureAuthenticated(req, res, next) {
+    if (req.session.user) {
+        return next();
+    }
+    res.status(401).json({
+        error: 'Unauthorized',
+        message: 'Unauthorized. Please log in.',
+        i18nKey: 'error_unauthorized'
+    });
+}
 
 function ensureAuthenticated(req, res, next) {
-    if (!req.session.user) {
-        return res.status(401).json({
-            error: 'Unauthorized',
-            message: 'Unauthorized. Please log in.',
-            i18nKey: 'error_unauthorized'
-        });
-    }
-
     if (req.session.user.is_banned) {
         const banReason = req.session.user.ban_reason;
-        const i18n = req.app.get('i18next');
-        const reasonText = banReason || i18n.t('ban_reason_not_specified');
-
-        req.session.destroy((err) => {
-            if (err) {
-                console.error("Session destruction error:", err);
-                return res.status(500).json({ error: 'Internal Server Error' });
-            }
-            return res.status(403).json({
-                error: 'Forbidden',
-                i18nKey: 'error_account_banned_with_reason',
-                options: { reason: reasonText }
-            });
+        req.session.destroy();
+        return res.status(403).json({
+            error: 'Forbidden',
+            i18nKey: 'error_account_banned_with_reason',
+            options: { reason: banReason || i18next.t('ban_reason_not_specified', { ns: 'translation'}) }
         });
-    } else {
-        next();
     }
+    res.status(401).json({ error: 'Unauthorized', i18nKey: 'error_unauthorized' });
 }
 
 module.exports = {
     ensureAuthenticated,
     ensureAdmin
-};
+}
